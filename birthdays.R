3f7fa2c0f1aeebe7eb7d3bffd9c96b026ff19e59
hasibekahraman <hasibe.kahraman@hotmail.com>
2019-09-12 14:46:22 -0400

    update information

--- birthdays.R
+++ birthdays.R
@@ -3,9 +3,10 @@ library(ggplot2)
 # What is your name?
 
 # When is your birthdate? Only Month and day.
+
 # Format "Month/date". For example for date "January 9th", we write "01/09"
-my_name <- NULL
-my_bd <- NULL
+my_name <- Hasibe
+my_bd <- 04/12
 
 
 
@@ -20,11 +21,11 @@ df <- df[order(df$birthdate) , ]
 
 same_bday <- df$birthdate[duplicated(df$birthdate)]
 
-df %>% ggplot(mapping = aes(x = reorder(names, as.numeric(birthdate)), 
-                            y = birthdate ) ) + 
+df %>% ggplot(mapping = aes(x = reorder(names, as.numeric(birthdate)),
+                            y = birthdate ) ) +
         geom_point() +
-        xlab("Names") + 
-        ylab("Birthdates") +  
-        labs(title = "Figure 1: Birthday of Participants", 
+        xlab("Names") +
+        ylab("Birthdates") +
+        labs(title = "Figure 1: Birthday of Participants",
              subtitle = "Anybody with the same birthday?")+
         geom_hline(yintercept = same_bday, color = "red", show.legend=T)
