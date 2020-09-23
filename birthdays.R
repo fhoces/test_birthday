1a0ed44613e46242ecc56c9d49d4972d183f1b72
Fernando Hoces de la Guardia <fhoces@users.noreply.github.com>
2020-09-22 20:08:58 -0700

    clean up before tutorial

--- birthdays.R
+++ birthdays.R
@@ -1,20 +1,15 @@
 library(ggplot2)
 library(magrittr)
 
-# What is your name? 
-Grace Morgan 
+# What is your name?
+my_name[1] <- "Catlin"
+
 
 # When is your birthdate? Only Month and day.
+# month first, then day
+my_bd[1] <- "03/05"
 
 # Format "Month/date". For example for date "January 9th", we write "01/09"
-my_name[3] <- "Hasibe"
-my_bd[3] <- "04/12"
-my_name[1] <- "Catlin"
-my_bd[1] <- "03/05"
-my_name[2] <- "matt"
-my_bd[2] <- "03/15"
-my_name[4] <- "Grace" 
-my_bd[4] <- "05/18"
 
 
 
